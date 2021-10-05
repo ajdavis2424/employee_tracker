03a2bb61f74289c68abf46cae46be1c4051d11a9
ALexander Davis <ajdavis2424@gmail.com>
2021-10-05 15:33:37 -0400

    createsd db folder and schema file & code

--- server.js
+++ server.js
@@ -1,15 +1,28 @@
 // npm init/inquirer(npm install inquirer) /express (npm i express)/mysql2 (npm install --save mysql2)
+// Installed CONSOLE TABLE PACKAGE (this allosw me to print myql rows to console) --> npm install console.table --save  // bower install console.table --save
 
+// MYSQL login -- mysql -u root -p
+// MAKE DATABASE IN TERMINAL
+
+const express = require('express');
 //INIT EXPRESS
 const app = express();
 
+// IMPORT & REQUIRE MYSQL2
+const mysql = require('mysql2');
+
+const PORT = process.env.PORT || 3306;
 // CREATE ENDPOINTS/ROUTE HANDLERS--- accepting get request to an index route, which is the slash
-app.get(`/`, function (req,res) {
-res.send(`Hello World`);
-});
+app.get('/', (req, res) => res.send('Navigate to /send or /routes'));
+
+app.get('/send', (req, res) =>
+  res.sendFile(path.join(__dirname, 'public/sendFile.html'))
+);
 
 //Listen on Port
-app.listen(5000);
+app.listen(PORT, () => {
+    console.log(`server running on port ${PORT}`)
+});
 
 // get the client
 const mysql = require('mysql2');
